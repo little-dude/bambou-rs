23782ea1e5414fcbec04e7420d58113e2ad08b4d
Corentin Henry <corentinhenry@gmail.com>
2017-06-01 18:43:51 -0700

    rename error::Request -> error:Reqwest

--- src/error.rs
+++ src/error.rs
@@ -5,7 +5,7 @@ use hyper;
 #[derive(Debug)]
 pub enum Error {
     InvalidUrl(hyper::error::ParseError),
-    Request(reqwest::Error),
+    Reqwest(reqwest::Error),
     MissingId,
     NoEntity,
     NoSession,
@@ -15,7 +15,7 @@ impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             Error::InvalidUrl(ref e) => fmt::Display::fmt(e, f),
-            Error::Request(ref e) => fmt::Display::fmt(e, f),
+            Error::Reqwest(ref e) => fmt::Display::fmt(e, f),
             Error::MissingId => f.write_str("The entity does not have an ID"),
             Error::NoEntity => f.write_str("No entity in response body"),
             Error::NoSession => f.write_str("Entities must hold a reference to a session to perform ReST requests"),
@@ -27,7 +27,7 @@ impl error::Error for Error {
     fn description(&self) -> &str {
         match *self {
             Error::InvalidUrl(ref e) => e.description(),
-            Error::Request(ref e) => e.description(),
+            Error::Reqwest(ref e) => e.description(),
             Error::MissingId => "The entity does not have an ID",
             Error::NoEntity => "No entity in response body",
             Error::NoSession => "Entities must hold a reference to a session to perform ReST requests",
@@ -37,7 +37,7 @@ impl error::Error for Error {
     fn cause(&self) -> Option<&error::Error> {
         match *self {
             Error::InvalidUrl(ref e) => Some(e),
-            Error::Request(ref e) => Some(e),
+            Error::Reqwest(ref e) => Some(e),
             _ => None,
         }
     }
@@ -45,7 +45,7 @@ impl error::Error for Error {
 
 impl From<reqwest::Error> for Error {
     fn from(err: reqwest::Error) -> Self {
-        Error::Request(err)
+        Error::Reqwest(err)
     }
 }
 
